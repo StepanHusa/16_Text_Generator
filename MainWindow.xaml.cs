4bab5ae750d5316dd94088b74223c77ea2ff7d70
StepanHusa <stepan.husa@gmail.com>
2021-12-27 23:26:48 +0100

    2021-12-27_23:26

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Diagnostics;
+using System.    IO;
 
 namespace _16_Text_Generator
 {
@@ -30,9 +31,10 @@ namespace _16_Text_Generator
             InitializeComponent();
 
             date.Text = DateTime.Now.ToShortDateString();
+            time.Text = DateTime.Now.ToShortTimeString();
 
 
-            Clipboard.SetText(DateTime.Now.ToShortDateString());
+            Clipboard.SetText(DateTime.Now.ToShortDateString()+"_"+DateTime.Now.ToShortTimeString());
 
 
             timer.Interval = 20000; //5 sec
@@ -40,6 +42,7 @@ namespace _16_Text_Generator
             timer.Start();
 
             this.WindowState = WindowState.Minimized;
+            FocusToPreviousWindow();
         }
 
         private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
@@ -56,7 +59,56 @@ namespace _16_Text_Generator
         {
             timer.Stop();
         }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(DateTime.Now.ToShortTimeString());
 
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var rand = new Random();
+            string str= ((int)Math.Round(Math.Pow(10, 8) * rand.NextDouble())).ToString();
+            Clipboard.SetText(str);
+            filename.Text = str;
+        }
+        #region focus property
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetLastActivePopup(IntPtr hWnd);
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        const uint GA_PARENT = 1;
+        const uint GA_ROOT = 2;
+        const uint GA_ROOTOWNER = 3;
+
+        public IntPtr GetPreviousWindow()
+        {
+            IntPtr activeAppWindow = GetForegroundWindow();
+            if (activeAppWindow == IntPtr.Zero)
+                return IntPtr.Zero;
+
+            IntPtr prevAppWindow = GetLastActivePopup(activeAppWindow);
+            return IsWindowVisible(prevAppWindow) ? prevAppWindow : IntPtr.Zero;
+        }
+
+        public void FocusToPreviousWindow()
+        {
+            IntPtr prevWindow = GetPreviousWindow();
+            if (prevWindow != IntPtr.Zero)
+                SetForegroundWindow(prevWindow);
+        }
+        #endregion
     }
 }
 
